47c71cf9eba83d7d74e3f1259dae4ed5331f08e9
Justin Lampe <xC0dex@users.noreply.github.com>
2025-08-27 11:19:58 +0200

    Add NSwag

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -11,16 +11,20 @@ builder.Services.AddSwaggerGen(options =>
     var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
     options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
 app.MapOpenApi();
-app.MapSwagger();
+app.MapSwagger("/swashbuckle/{documentName}.json");
+app.UseOpenApi(options => options.Path = "/nswag/{documentName}.json");
+
 app.MapScalarApiReference(options =>
 {
     options
         .AddDocument("v1", "Microsoft")
-        .AddDocument("v1", "Swashbuckle", "/swagger/{documentName}/swagger.json");
+        .AddDocument("v1", "Swashbuckle", "/swashbuckle/{documentName}.json")
+        .AddDocument("v1", "NSwag", "/nswag/{documentName}.json");
 });
 
 app.MapWeatherForecastEndpoints();
